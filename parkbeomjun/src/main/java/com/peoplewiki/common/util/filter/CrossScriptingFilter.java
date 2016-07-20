edde6c4bde44d5e40f034c2a4f6daca3dcaafc30
hanwha_park <cain007@naver.com>
2016-07-20 13:27:42 +0900

    크로스 스크립팅 다시 커밋

--- parkbeomjun/src/main/java/com/peoplewiki/common/util/filter/CrossScriptingFilter.java
+++ parkbeomjun/src/main/java/com/peoplewiki/common/util/filter/CrossScriptingFilter.java
@@ -1,6 +1,7 @@
 package com.peoplewiki.common.util.filter;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -8,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class CrossScriptingFilter implements Filter {
 
@@ -22,7 +24,16 @@ public class CrossScriptingFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		chain.doFilter(new Wrapper((HttpServletRequest) request), response);
+		
+		// 크로스~~~~~~~~~~
+		System.out.println("Filtering on...........................................................");
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(request, response);
+        
 
 	}
 
