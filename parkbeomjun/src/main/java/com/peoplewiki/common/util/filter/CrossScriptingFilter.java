0003ce6f5824e0d55257b9510ba5e251e7b84ea9
hanwha_park <cain007@naver.com>
2016-07-20 13:26:45 +0900

    Revert "크로스스크립트 테스트로 수정해봄"
    This reverts commit c75adc917cfe5f1debb65b77f90447655284cca5.

--- parkbeomjun/src/main/java/com/peoplewiki/common/util/filter/CrossScriptingFilter.java
+++ parkbeomjun/src/main/java/com/peoplewiki/common/util/filter/CrossScriptingFilter.java
@@ -1,7 +1,6 @@
 package com.peoplewiki.common.util.filter;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 public class CrossScriptingFilter implements Filter {
 
@@ -24,16 +22,7 @@ public class CrossScriptingFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
-		// 크로스~~~~~~~~~~
-		System.out.println("Filtering on...........................................................");
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-        res.setHeader("Access-Control-Max-Age", "3600");
-        res.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        chain.doFilter(request, response);
-        
+		chain.doFilter(new Wrapper((HttpServletRequest) request), response);
 
 	}
 
